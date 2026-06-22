6e340d9cd31732e59e9a6bff50a2ac5e1b14454e
Vivek Patel <VivekPatel@gmail.com>
2026-06-22 17:47:56 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index";
+
+dotenv.config({
+  path: './env'
+})
+
+
+
+connectDB()
+
+.then(()=>{
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is runnig at port ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MONGO db connection failed ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+;( async () => {
+  try{
+     await mongoose.connet(`${process.env.MONGODB_URI}/${DB_NAME}`)
+     app.on("error",(error)=>{
+      console.log("ERROR: ", error);
+      throw error
+     })
+
+     app.listen(process.env.PORT,()=>{
+      console.log(`App is listening on port  ${process.env.PORT}`)
+     })
+  }
+  catch(error)
+  {
+     console.error("ERROR: ", error)
+     throw err
+  }
+    
+} )()
+  
+*/
